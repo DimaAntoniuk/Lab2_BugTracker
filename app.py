f3c17fca9d641ac19261078d20408de7476564ef
Dima Antoniuk <dimaantoniuk09@gmail.com>
2019-09-11 10:48:29 +0300

    changed file input and output

--- app.py
+++ app.py
@@ -1,11 +1,3 @@
-class File(object):
-    def __init__(self, file_name, method):
-        self.file_obj = open(file_name, method)
-    def __enter__(self):
-        return self.file_obj
-    def __exit__(self, type, value, traceback):
-        self.file_obj.close()
-
 def max2(x, y):
     if(x > y):
         return x
@@ -14,7 +6,7 @@ def max2(x, y):
 
 numbers = []
 
-with File('bugtrk.in', 'r') as input:
+with open('bugtrk.in', 'r') as input:
     numbers = input.read().split()
 
 items_number = int(numbers[0])
@@ -29,5 +21,5 @@ while items_number_width*items_number_height < items_number:
     else:
         items_number_height += 1
 
-with File('bugtrk.out', 'w+') as output:
+with open('bugtrk.out', 'w+') as output:
     output.write(str(max2(items_number_width*item_width, items_number_height*item_height)))
